f756fe34772658c90cf56b1ee70bcd33b1f21986
Heath Attig <heath.attig@gmail.com>
2015-08-22 05:27:47 -0500

    split out files

---
+++ spec/sub_cache_spec.rb
@@ -0,0 +1,64 @@
+require_relative '../sub_cache'
+
+describe SubCache do
+  subject { SubCache.new('ID123', double) }
+
+  describe :new do
+
+    it 'sets the parent_key attribute' do
+      expect(subject.parent_key).to eq 'ID123'
+    end
+
+    it 'sets the parent_cache attribute' do
+      parent = double
+      cache = SubCache.new('ID123', parent)
+      expect(cache.parent_cache).to eq parent
+    end
+  end
+
+  describe :read do
+    it 'returns nil when key does not exist' do
+      expect(subject.read('non_existant_key')).to eq nil
+    end
+
+    it 'returns a value for the given key' do
+      subject.__setobj__({ 'my_key' => 'my_value' })
+      expect(subject.read('my_key')).to eq 'my_value'
+    end
+
+    it 'treats string keys and symbol keys as the same key' do
+      subject.__setobj__({ 'my_key' => 'my_value' })
+      expect(subject.read(:my_key)).to eq 'my_value'
+    end
+  end
+
+  describe :write do
+    it 'saves a value in the cache' do
+      subject.write('my_key','my_value')
+      expect(subject.read('my_key')).to eq 'my_value'
+    end
+
+    it 'converts symbol keys to strings' do
+      subject.write(:my_key, 'my_value')
+      expect(subject.__getobj__['my_key']).to eq 'my_value'
+    end
+  end
+
+  describe :__getobj__ do
+    it 'is defined' do
+      expect{subject.__getobj__}.not_to raise_error
+    end
+  end
+
+  describe :__setobj__ do
+    it 'is defined' do
+      expect{subject.__setobj__(double)}.not_to raise_error
+    end
+
+    it 'sets the delegate object' do
+      delegate = {}
+      subject.__setobj__(delegate)
+      expect(subject.__getobj__).to eq delegate
+    end
+  end
+end
--- sub_cache.rb
+++ sub_cache.rb
@@ -1,5 +1,4 @@
 require 'delegate'
-require 'rspec'
 
 class SubCache < Delegator
   attr_reader :parent_key, :parent_cache
@@ -11,11 +10,11 @@ class SubCache < Delegator
   end
 
   def read(key)
-    @cache.fetch(key, nil)
+    @cache.fetch(key.to_s, nil)
   end
 
   def write(key, value)
-    @cache.store(key, value)
+    @cache.store(key.to_s, value)
   end
 
   def __getobj__
@@ -24,59 +23,5 @@ class SubCache < Delegator
 
   def __setobj__(obj)
     @cache = obj
-  end
-end
-
-
-describe SubCache do
-  subject { SubCache.new('ID123', double) }
-
-  describe :new do
-
-    it 'sets the parent_key attribute' do
-      expect(subject.parent_key).to eq 'ID123'
-    end
-
-    it 'sets the parent_cache' do
-      parent = double
-      cache = SubCache.new('ID123', parent)
-      expect(cache.parent_cache).to eq parent
-    end
-  end
-
-  describe :read do
-    it 'returns nil when key does not exist' do
-      expect(subject.read('non_existant_key')).to eq nil
-    end
-
-    it 'returns a value for the given key' do
-      subject.__setobj__({ 'my_key' => 'my_value' })
-      expect(subject.read('my_key')).to eq 'my_value'
-    end
-  end
-
-  describe :write do
-    it 'saves a value in the cache' do
-      subject.write('key','value')
-      expect(subject.read('key')).to eq 'value'
-    end
-  end
-
-  describe :__getobj__ do
-    it 'is defined' do
-      expect{subject.__getobj__}.not_to raise_error
-    end
-  end
-
-  describe :__setobj__ do
-    it 'is defined' do
-      expect{subject.__setobj__(double)}.not_to raise_error
-    end
-
-    it 'sets the delegate object' do
-      delegate = {}
-      subject.__setobj__(delegate)
-      expect(subject.__getobj__).to eq delegate
-    end
   end
 end
