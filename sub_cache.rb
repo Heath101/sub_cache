b69120310b1fb130b6abcff93426075b9b7e7214
Heath Attig <heath.attig@gmail.com>
2015-08-22 04:56:57 -0500

    wip set attributes

--- sub_cache.rb
+++ sub_cache.rb
@@ -2,9 +2,12 @@ require 'delegate'
 require 'rspec'
 
 class SubCache < Delegator
+  attr_reader :parent_key, :parent_cache
 
-  def initialize
+  def initialize(parent_key, parent_cache)
     @store = {}
+    @parent_key = parent_key
+    @parent_cache = parent_cache
   end
 
   def __getobj__
@@ -19,23 +22,36 @@ end
 
 
 describe SubCache do
+  subject { SubCache.new('ID123', double) }
+
+  describe :new do
+
+    it 'sets the parent_key attribute' do
+      expect(subject.parent_key).to eq 'ID123'
+    end
+
+    it 'sets the parent_cache' do
+      parent = double
+      cache = SubCache.new('ID123', parent)
+      expect(cache.parent_cache).to eq parent
+    end
+  end
 
   describe :__getobj__ do
     it 'is defined' do
-      expect{SubCache.new.__getobj__}.not_to raise_error
+      expect{subject.__getobj__}.not_to raise_error
     end
   end
 
   describe :__setobj__ do
     it 'is defined' do
-      expect{SubCache.new.__setobj__(double)}.not_to raise_error
+      expect{subject.__setobj__(double)}.not_to raise_error
     end
 
     it 'sets the delegate object' do
       delegate = {}
-      cache = SubCache.new
-      cache.__setobj__(delegate)
-      expect(cache.__getobj__).to eq delegate
+      subject.__setobj__(delegate)
+      expect(subject.__getobj__).to eq delegate
     end
   end
 end
