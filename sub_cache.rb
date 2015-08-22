fa31c4ae85272565f94bbc198f9d61d3348287eb
Heath Attig <heath.attig@gmail.com>
2015-08-22 05:16:12 -0500

    wip reading and writing

--- sub_cache.rb
+++ sub_cache.rb
@@ -5,19 +5,26 @@ class SubCache < Delegator
   attr_reader :parent_key, :parent_cache
 
   def initialize(parent_key, parent_cache)
-    @store = {}
+    @cache = {}
     @parent_key = parent_key
     @parent_cache = parent_cache
   end
 
+  def read(key)
+    @cache.fetch(key, nil)
+  end
+
+  def write(key, value)
+    @cache.store(key, value)
+  end
+
   def __getobj__
-    @store
+    @cache
   end 
 
   def __setobj__(obj)
-    @store = obj
+    @cache = obj
   end
-
 end
 
 
@@ -34,6 +41,24 @@ describe SubCache do
       parent = double
       cache = SubCache.new('ID123', parent)
       expect(cache.parent_cache).to eq parent
+    end
+  end
+
+  describe :read do
+    it 'returns nil when key does not exist' do
+      expect(subject.read('non_existant_key')).to eq nil
+    end
+
+    it 'returns a value for the given key' do
+      subject.__setobj__({ 'my_key' => 'my_value' })
+      expect(subject.read('my_key')).to eq 'my_value'
+    end
+  end
+
+  describe :write do
+    it 'saves a value in the cache' do
+      subject.write('key','value')
+      expect(subject.read('key')).to eq 'value'
     end
   end
 
